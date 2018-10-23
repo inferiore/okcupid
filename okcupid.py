f185e50352ae1ff1d7e5f2df24a90591fd8f44da
ebarrios <ebarrios@zeus.co>
2018-10-23 12:55:51 -0500

    matriz of matches(both answers)

--- okcupid.py
+++ okcupid.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 #read file
 profiles = json.loads(open('input.json').read())
@@ -7,29 +8,32 @@ print("cantidad de profiles")
 #print(len(leer["profiles"][0]))
 #total points  by question macthed for each user
 total_importance=0
+#profile,other_profile,question_id,importance,acceptableAnswers
+matches = []
+points = []
+
 for profile in profiles["profiles"]: #all profiles
-    if(profile ["id"]==0):
+    if profile["id"] == 0:
         for question in profile["answers"]:#all question by profile
             for other_profile in profiles["profiles"]:#all question others profiles
-                aDict = {}
                 for index ,question_other_profile in enumerate(other_profile["answers"]):#all question of other profile
-                    if(question["questionId"]==question_other_profile["questionId"] and profile["id"]==other_profile["id"] ):
-                          #if both responce the same question calculated all points posibles
-                        aDict[question["importance"]]=(question["importance"])
-                print(aDict)
+                    if question["questionId"] == question_other_profile["questionId"] and profile["id"] != other_profile["id"] :
+                        #if both responce the same question calculated all points posibles
+                        matches.append([profile["id"],other_profile["id"],question["questionId"],question["importance"],question["acceptableAnswers"]])
                 #print(total_importance)
                 #profile[other_profile["id"]]=total_importance
                 #print (other_profile["id"])
 
 
-        print("n\id:",profile ["id"],profile)
+        #print("n\id:",profile ["id"],profile)
+#total point and real point
+#for match in matches:
+#    print("n\match:",match)
+#    points.append([match[0],])
 
-list = [1,2]
-list2 = [1,2]
+#array = np.array([[1, 0, -1], [3, 4, 1]])
+#salida = np.sum(array, axis=1)
+#print(salida)
 
-def sumar(list):
-    list_sum = 0
-    for x in list:
-        list_sum += x
-    return list_sum
+
 
